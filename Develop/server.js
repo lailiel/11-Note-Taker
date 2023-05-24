5d46f65753aa07bdc24b2e887990a5d8aa81b394
Abi L <124858748+lailiel@users.noreply.github.com>
2023-05-23 21:34:33 -0500

    notes are properly populating in database. but aren't displaying on page. working on uuid so notes display properly

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const app = express();
 const PORT = 3001;
 const notes = require('./db/db.json');
+const uuid = require('./helpers/uuid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,15 +24,68 @@ app.get('/notes', (req, res) => {
 });
 
 // get api/notes db.json
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(notes);
+});
 
 
 
 // post api/notes db.json
 app.post('/api/notes', (req, res) => {
-  // Let the client know that their POST request was received
-  res.json(`${req.method} request received`);
-  console.info(`${req.method} request received`);
+
+  // ---------------------------------------------------------------
+
+    const { title, text } = req.body;
+  
+    if (title && text ) {
+
+      const newNote = {
+        title,
+        text,
+        note_id,
+      };
+  
+      // ----------------------
+  
+
+      fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new review
+          parsedNotes.push(newNote);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated Notes!')
+          );
+        }
+      });
+  
+
+    // ---------------------
+
+
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting review');
+    }
+  
+  // ---------------------------------------------------------------
 });
 
 // bonus: delete api/notes db.json id property db.json
