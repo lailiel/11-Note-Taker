a5d63dd29a67a22ae90cd1d791c6653b069492fe
Abi L <124858748+lailiel@users.noreply.github.com>
2023-05-23 22:04:47 -0500

    Note should work in theory, notes are still not dipslaying on page but think issue might resolve itself when hosted on heroku. will return to fix if i'm wrong

--- Develop/server.js
+++ Develop/server.js
@@ -1,101 +1,75 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 const PORT = 3001;
-const notes = require('./db/db.json');
-const uuid = require('./helpers/uuid');
+const notes = require("./db/db.json");
+const uuid = require("./helpers/uuid");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-
-  // get /notes notes.html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-// get api/notes db.json
-app.get('/api/notes', (req, res) => {
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
   res.status(200).json(notes);
 });
 
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
 
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
 
-// post api/notes db.json
-app.post('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
 
-  // ---------------------------------------------------------------
+        parsedNotes.push(newNote);
 
-    const { title, text } = req.body;
-  
-    if (title && text ) {
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated Notes!")
+        );
+      }
+    });
 
-      const newNote = {
-        title,
-        text,
-        note_id,
-      };
-  
-      // ----------------------
-  
+    const response = {
+      status: "success",
+      body: newNote,
+    };
 
-      fs.readFile('./db/db.json', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-  
-          // Add a new review
-          parsedNotes.push(newNote);
-  
-          // Write updated reviews back to the file
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNotes, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated Notes!')
-          );
-        }
-      });
-  
-
-    // ---------------------
-
-
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in posting review');
-    }
-  
-  // ---------------------------------------------------------------
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting review");
+  }
 });
 
-// bonus: delete api/notes db.json id property db.json
+app.get("/api/notes", (req, res) => {
+  res.status(200).json(notes);
+});
 
-
-
-
-// get * index.html
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
