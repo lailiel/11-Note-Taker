d23b6a8b4ab56cd7f52dded1ca231bbfd4a49433
Abi L <124858748+lailiel@users.noreply.github.com>
2023-05-25 20:46:35 -0500

    added delete functionality

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  
   var currentData;
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
@@ -70,13 +69,48 @@ app.post("/api/notes", (req, res) => {
     };
 
     console.log(response);
-    // work from here
-
     res.status(200).json(response);
   } else {
     res.status(500).json("Error in posting note");
   }
 });
+
+// DELETE -----------------------
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+        const filteredNotes = parsedNotes.filter((data) => data.id !== id);
+
+        fs.writeFile(
+          "./db/db.json",
+
+          JSON.stringify(filteredNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully deleted Note!")
+        );
+      }
+    });
+
+    const response = {
+      status: "success",
+    };
+
+    console.log(response);
+    res.status(200).json(response);
+  } else {
+    res.status(500).json("Error in deleting note");
+  }
+});
+
+// ---------------
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
